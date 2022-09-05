33d447213ee85f448195fd9c11b4016c40aadf15
Marcos Rolando <marcosrolando.mr@gmail.com>
2022-09-05 16:59:01 -0300

    Add person data to script

--- make-compose.py
+++ make-compose.py
@@ -1,4 +1,28 @@
+from dataclasses import dataclass
+import random
 import sys
+import string
+import time
+import datetime
+
+random.seed(time.time())
+
+@dataclass
+class PersonData():
+  name: str
+  surname: str
+  document: str
+  birthdate: str
+
+  def get_random():
+    names = ['Julio', 'Mateo', 'Claudio', 'Pablo', 'Juan', 'Federico']
+    surnames = ['Gutierrez', 'Rodriguez', 'Alvarez', 'Bonifetto', 'Ortiz', 'Gallo']
+    return PersonData(
+      name=random.choice(names),
+      surname=random.choice(surnames),
+      document=''.join(random.choice(string.digits) for _ in range(8)),
+      birthdate=datetime.date.fromtimestamp(random.randint(1, int(time.time()))).strftime('%Y-%m-%d')
+    )
 
 try:
   clientCount = int(sys.argv[1])
@@ -29,6 +53,7 @@ services:
 )
 
 for i in range(1, clientCount + 1):
+  pd = PersonData.get_random()
   file.write(
   """
   client{clientId}:
@@ -40,13 +65,18 @@ for i in range(1, clientCount + 1):
       - CLI_SERVER_ADDRESS=server:12345
       - CLI_LOOP_LAPSE=1m2s
       - CLI_LOG_LEVEL=DEBUG
+      - CLI_PERSON_NAME={name}
+      - CLI_PERSON_SURNAME={surname}
+      - CLI_PERSON_DOCUMENT={document}
+      - CLI_PERSON_BIRTHDATE={birthdate}
     networks:
       - testing_net
     depends_on:
       - server
     volumes:
       - ./client/config.yaml:/config.yaml
-  """.format(clientId=i)
+  """.format(clientId=i, name=pd.name, surname=pd.surname, 
+            document=pd.document, birthdate=pd.birthdate)
   )
 
 file.write(

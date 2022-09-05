0b5ca808cd452bea1923f8ec41747265a3b4bd2c
Marcos Rolando <marcosrolando.mr@gmail.com>
2022-09-04 23:23:27 -0300

    Handle errors in client

--- client/common/client.go
+++ client/common/client.go
@@ -1,12 +1,9 @@
 package common
 
 import (
-	// "bufio"
-	// "fmt"
-	// "net"
-	// "os"
-	// "os/signal"
-	// "syscall"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -25,7 +22,6 @@ type ClientConfig struct {
 type Client struct {
 	config     ClientConfig
 	lottery 	*LotteryConnection
-	// conn   net.Conn
 }
 
 // NewClient Initializes a new client receiving the configuration
@@ -33,7 +29,6 @@ type Client struct {
 func NewClient(config ClientConfig) *Client {
 	client := &Client{
 		config:     config,
-		lottery: nil,
 	}
 	return client
 }
@@ -48,81 +43,38 @@ func (c *Client) PlayLottery() {
 		)
 	}
 	c.lottery = lotteryConn
+	defer c.lottery.Close() // Ignoring error as we are already closing the program, nothing to be done
 
-	defer func() {
-		c.lottery.Close() // TODO handle error
-		c.lottery = nil
+	sigtermSignal := make(chan os.Signal, 1)
+	signal.Notify(sigtermSignal, syscall.SIGTERM)
+	defer	func() {
+		signal.Stop(sigtermSignal)
+		close(sigtermSignal)
+	}()
+	go func() {
+		_, ok := <- sigtermSignal
+		if ok {
+			log.Infof("[CLIENT %v] Received SIGTERM signal, terminating client...", c.config.ID)
+			c.lottery.Close() // TODO see about os.exit(0) maybe thread leak? race with panic maybe?
+		}
 	}()
 
 	c.submitDataToLottery()
 	c.checkLotteryResult()
-
-	// TODO expect response
 }
 
 func (c* Client) submitDataToLottery() {
-	c.lottery.SendPersonInfo(c.config.PersonInfo)
-	// TODO handle possible error
+	if err := c.lottery.SendPersonInfo(c.config.PersonInfo); err != nil {
+		log.Panicf("[CLIENT %v] Failed to submit data to Lottery. Error: %v", c.config.ID, err.Error())
+	}
+	log.Infof("[CLIENT %v] Person data submitted to the Lottery", c.config.ID)
 }
 
 func (c* Client) checkLotteryResult() {
 	result, err := c.lottery.GetResult()
 	if err != nil {
-		// TODO handle error
+		log.Panicf("[CLIENT %v] Failed to get result from Lottery. Error: %v", c.config.ID, err.Error())
+	} else {
+		log.Infof("[CLIENT %v] The result of the lottery is: %v", c.config.ID, result)
 	}
-	log.Infof("[CLIENT %v] The result of the lottery is: %v", c.config.ID, result)
 }
-
-// // StartClientLoop Send messages to the client until some time threshold is met
-// func (c *Client) StartClientLoop() {
-// 	// Create the connection the server in every loop iteration. Send an
-// 	// autoincremental msgID to identify every message sent
-// 	c.createClientSocket()
-// 	msgID := 1
-
-// 	sigtermSignal := make(chan os.Signal, 1) // TODO add this to the PlayLottery method
-// 	signal.Notify(sigtermSignal, syscall.SIGTERM)
-
-// loop:
-// 	// Send messages if the loopLapse threshold has been not surpassed
-// 	for timeout := time.After(c.config.LoopLapse); ; {
-// 		select {
-// 		case <-timeout:
-// 			break loop
-// 		case <-sigtermSignal:
-// 			break loop
-// 		default:
-// 		}
-
-// 		// Send
-// 		fmt.Fprintf(
-// 			c.conn,
-// 			"[CLIENT %v] Message N°%v sent\n",
-// 			c.config.ID,
-// 			msgID,
-// 		)
-// 		msg, err := bufio.NewReader(c.conn).ReadString('\n')
-// 		msgID++
-
-// 		if err != nil {
-// 			log.Errorf(
-// 				"[CLIENT %v] Error reading from socket. %v.",
-// 				c.config.ID,
-// 				err,
-// 			)
-// 			c.conn.Close()
-// 			return
-// 		}
-// 		log.Infof("[CLIENT %v] Message from server: %v", c.config.ID, msg)
-
-// 		// Wait a time between sending one message and the next one
-// 		time.Sleep(c.config.LoopPeriod)
-
-// 		// Recreate connection to the server
-// 		c.conn.Close()
-// 		c.createClientSocket()
-// 	}
-
-// 	log.Infof("[CLIENT %v] Closing connection", c.config.ID)
-// 	c.conn.Close()
-// }
--- client/common/lottery_connection.go
+++ client/common/lottery_connection.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"net"
 )
@@ -9,12 +10,13 @@ import (
 type LotteryResult string
 
 const (
-	Loser 	LotteryResult = "loser"
-	Winner	LotteryResult	= "winner"
+	Loser 	LotteryResult = "Better luck next time... Loser"
+	Winner	LotteryResult	= "Winner winner chicken dinner, you won!"
 )
 
 type LotteryConnection struct {
-	conn net.Conn
+	conn 			net.Conn
+	sentInfo	bool
 }
 
 func NewLotteryConnection(address string) (*LotteryConnection, error) {
@@ -24,31 +26,38 @@ func NewLotteryConnection(address string) (*LotteryConnection, error) {
 	}
 	return &LotteryConnection{
 		conn,
+		false,
 	}, nil
 }
 
 func (lc *LotteryConnection) SendPersonInfo(info PersonData) error {
 	encodedInfo := fmt.Sprintf("%s;%s;%s;%s", info.Name, info.Surname, 
-														info.Document, info.Birthdate) // TODO encapsulate data serialization
+														info.Document, info.Birthdate)
 	encodedInfoLength := uint16(len(encodedInfo))
 	buffer := make([]byte, encodedInfoLength + 2) // + 2 for the length bytes
 	binary.BigEndian.PutUint16(buffer, encodedInfoLength)
 	copy(buffer[2:], []byte(encodedInfo))
 	_, err := lc.conn.Write(buffer) // TODO ver el n que devuelve
+	if err == nil { lc.sentInfo = true }
 	return err
 }
 
 func (lc *LotteryConnection) GetResult() (LotteryResult, error) {
-	buffer := make([]byte, 1)
-	_, err := lc.conn.Read(buffer) // TODO check n
-	if err != nil {
-		return Loser, err
+	if !lc.sentInfo {
+		return "", errors.New("person info was not previously sent to the Lottery")
 	}
-	if buffer[0] != 0 && buffer[0] != 1 {
-		// TODO handle error
+	buffer := make([]byte, 1)
+	// TODO check n
+	if _, err := lc.conn.Read(buffer); err != nil {
+		return "", err
+	}
+	resCode := buffer[0]
+	isValidResponse := func() bool { return resCode == 0 || resCode == 1 };
+	if !isValidResponse() {
+		panic(fmt.Sprintf("Received an invalid lottery response status of %v", resCode))
 	}
 	resultMap := map[byte]LotteryResult{0: Loser, 1: Winner}
-	return resultMap[buffer[0]], nil
+	return resultMap[resCode], nil
 }
 
 func (lc *LotteryConnection) Close() error {
